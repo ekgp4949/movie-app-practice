55d8893534d57b753388f3a62ae38c6b8849f315
ekgp4949 <ekgp4949@gmail.com>
2021-10-28 22:58:06 +0900

    [mod] react 연습

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-function Food({ id, name, rating }) {
-  return <h2>I like {name}, {rating}</h2>
+class App extends React.Component {
+
+  state = {
+    count: 9
+  }
+
+  componentDidMount() {}
+
+  add = () => this.setState(current => ({count: current.count+1}) );
+
+  minus = () => this.setState(current => ({count: current.count-1}) );
+
+  render() {
+    return (
+      <div>
+        <h1>hihi: { this.state.count }</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
-
-const foodILike = [
-  {id:1, name:"a", rating: 5.0},
-  {id:2, name:"b", rating: 4.9}
-];
-
-function App() {
-  return (
-  <div className="App">
-    { foodILike.map((dish) => <Food key={dish.id} name={dish.name} rating={dish.rating}/>) }
-  </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  rating: PropTypes.number //평점이 등록되지 않았을 수도 있음
-};
 
 export default App;
