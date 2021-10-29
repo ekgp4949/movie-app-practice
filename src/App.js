fc5f684698498fcf55b695d5a6e7bb2088027152
ekgp4949 <ekgp4949@gmail.com>
2021-10-29 23:37:10 +0900

    [mod] axios 적용 및 async, await 사용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
 
@@ -7,17 +8,24 @@ class App extends React.Component {
     movies: []
   }
 
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    this.setState({ isLoading: false, movies });
+  }
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading : false, movies: [1,2,3,4,5,6] })
-    }, 3000);
+    this.getMovies();
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
       <div>
-        {isLoading ? 'Loading....' : this.state.movies.map(item => <div>Movie{item}</div>)}
+        {isLoading ? 'Loading....' : movies.map(item => <div>Movie {item.id}</div>)}
       </div>
     );
   }
