b3b8e6726e6139c725a0d83fca84f53ddfd462d1
ekgp4949 <ekgp4949@gmail.com>
2021-11-01 23:08:52 +0900

    [add] About, Home 컴포넌트 생성

---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./About.css"
+
+function About() {
+  return (
+    <div className="about__container">
+      <span>
+        "Freedom is 어쩌구"
+      </span>
+      <span>다혜 킴, 2021</span>
+    </div>
+  );
+}
+
+export default About;
